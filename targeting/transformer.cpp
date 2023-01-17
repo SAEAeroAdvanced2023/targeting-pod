6731d173e08fc913796db82eddda93dd29263c5a
xaavian <xaavian@hotmail.com>
2023-01-17 12:32:43 -0500

    Fixed bugs from preliminary testing

--- targeting/transformer.cpp
+++ targeting/transformer.cpp
@@ -8,7 +8,7 @@ using namespace std;
 Eigen::MatrixXd transformation_translation(double x, double y, double z){
     Eigen::MatrixXd t(4,4);
     t << 1, 0, 0, -x, 0, 1, 0, -y, 0, 0, 1, -z, 0, 0, 0, 1;
-    return t.inverse();
+    return t;
 }
 
 Eigen::MatrixXd transformation_rotation(double a, double b, double c){
@@ -33,7 +33,9 @@ Eigen::MatrixXd intersection(Eigen::MatrixXd p0, Eigen::MatrixXd p1, Eigen::Vect
     } else {
         // Segment effectively parallel to plane uWu
         Eigen::MatrixXd x(1,3);
-        x << 0, 0, 0; // This might break something if ever we get a line parallel to the ground, hope it doesn't :)
+        // This might break something if ever we get a line parallel to the ground, hope it doesn't :)
+        // TODO: Make sure that a coordinate of <0,0,0> is handled PROPERLY somewhere in the code
+        x << 0, 0, 0;
         return x;
     }
 
@@ -65,10 +67,10 @@ void transform(Eigen::MatrixXd v_dist, double roll, double yaw, double pitch, do
     cc << 0, -1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1;
     // Pitch matrix (90 deg around y-axis)
     Eigen::MatrixXd cam(4,4);
-    cc << 0, 0, 1, 0, 0, 1, 0, 0, -1, 0, 0, 0, 0, 0, 0, 1;
+    cam << 0, 0, 1, 0, 0, 1, 0, 0, -1, 0, 0, 0, 0, 0, 0, 1;
     // Depth matrix (For depth of object in sensor frame w.r.t. camera frame)
     Eigen::MatrixXd im(4,4);
-    cc << f, 0, 0, 0, 0, f, 0, 0, 0, 0, f, 0, 0, 0, 0, 1;
+    im << f, 0, 0, 0, 0, f, 0, 0, 0, 0, f, 0, 0, 0, 0, 1;
 
     // Rotation and Translation from inertial frame to camera frame
     Eigen::MatrixXd trans_c = transformation_translation(c_dist(0,0), c_dist(0,1), c_dist(0,2));
@@ -80,15 +82,22 @@ void transform(Eigen::MatrixXd v_dist, double roll, double yaw, double pitch, do
     // Camera coordinates w.r.t. inertial frame
     Eigen::MatrixXd p_cc = trans_i.inverse() * rot_v * trans_g.inverse() * rot_g * trans_c.inverse() * c;
     // Coordinates of object seen in the camera sensor frame wrt inertial frame
+
+    // TODO: trans_i wrong, idk why yet!,
+    cout << trans_i << endl;
+
     Eigen::MatrixXd q_obj = trans_i.inverse() * rot_v * trans_g.inverse() * rot_g * trans_c.inverse() * cam * im * cc * ccm_inv * pix;
     // Intersection point between line formed with the p_cc and q_obj and plane (ground), represents the location of the target w.r.t. the inertial frame
     Eigen::Vector3d p_cc_x(3);
     p_cc_x << p_cc(0,0), p_cc(1,0), p_cc(2,0);
     Eigen::Vector3d q_obj_x(3);
     q_obj_x << q_obj(0,0), q_obj(1,0), q_obj(2,0);
+
+    //cout << p_cc_x << "\n\n" << q_obj_x << "\n\n" << gnd.row(0) << "\n\n" << gnd.row(1) << endl;
+
     Eigen::MatrixXd t = intersection(p_cc_x, q_obj_x, gnd.row(0), gnd.row(1));
 
-    // cout << t << endl;
+    cout << t << endl;
     // TODO: No validation being performed here, maybe worth returning a struct including how certain it is that it's a good point along with the coordinates
     // return t;
 
